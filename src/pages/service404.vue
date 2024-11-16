c445cd9d622a6973106e471a39e78c9cd79206ef
librelol <hello@libre.lol>
2024-11-16 17:58:11 -0500

    Added a tracker for 404 page to hopefully later on provide feedback on if the service is down

--- src/pages/service404.vue
+++ src/pages/service404.vue
@@ -23,6 +23,20 @@
           </v-btn>
         </v-col>
       </v-row>
+
+      <!-- Displaying Referrer Info (for Debugging) -->
+      <v-row justify="center" class="mt-5">
+        <v-col cols="12" md="8" lg="6">
+          <v-alert type="info" elevation="2" class="mb-5" border="left">
+            <v-icon left>mdi-information-outline</v-icon>
+            <div>
+              <h4 class="headline">Referrer Info:</h4>
+              <p>{{ referrer ? `You came from: ${referrer}` : 'No referrer available.' }}</p>
+            </div>
+          </v-alert>
+        </v-col>
+      </v-row>
+
     </v-container>
   </v-app>
 </template>
@@ -30,9 +44,21 @@
 <script>
 export default {
   name: 'NotFoundPage',
+  data() {
+    return {
+      referrer: document.referrer || null, // Capture the referrer URL (if available)
+    };
+  },
   methods: {
     goHome() {
       this.$router.push('/');
+    }
+  },
+  mounted() {
+    // Optionally, you could send the referrer to your analytics service or log it
+    if (this.referrer) {
+      console.log('Visitor came from:', this.referrer); // Log the referrer to the console (for debugging purposes)
+      // You could send this information to a server, analytics service, etc.
     }
   }
 }
